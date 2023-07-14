b3437d92c40962338e12d7a5b579aa7f3799030c
quinoesteban555 <69019687+quinoesteban555@users.noreply.github.com>
2023-07-14 14:29:18 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -24,3 +24,32 @@ const PORT = 5001;
 app.listen(PORT, () =>
   console.log(`Server is running on port: http://localhost:${PORT}...`)
 );
+class PricingModule{
+    Gallons = 0;
+    Name = '';
+    State = 'TX';
+    Address = '';
+    Date = '';
+    SuggestedPrice = 0;
+    TotalPrice = 0;
+    setName(Name)
+    {
+        this.Name = Name;
+    }
+    setState(State)
+    {
+        this.State = State;
+    }
+    setGallons(Gallons)
+    {
+        this.Gallons = Gallons;
+    }
+    setAdress(Address)
+    {
+        this.Address = Address;
+    }
+    setSuggestedPrice(SuggestedPrice)
+    {
+        this.SuggestedPrice = SuggestedPrice;
+    }
+}; 
