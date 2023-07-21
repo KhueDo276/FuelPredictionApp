8c86d08fa9f270b8de27290710bfe9afe158ce23
quinoesteban555 <69019687+quinoesteban555@users.noreply.github.com>
2023-07-21 11:00:51 -0500

    Update fuelForm.js

--- client/src/components/fuelForm.js
+++ client/src/components/fuelForm.js
@@ -2,87 +2,80 @@ import React, { Component } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "./styles/fuelForm.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Navbar from "./navbar";
 
-class Pricing extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { gallons: "", address: "", date: "", suggestedprice: "" };
-  }
-  handleInputChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    this.setState({ [name]: value });
-  };
-  handleChange = (date) => {
-    this.setState({ date });
-  };
-  handleSubmit = (event) => {
+
+  
+function Pricing() {
+    
+  const [gallons, setGallons] = useState("");
+
+  const [address, setAddress] = useState("#");
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [quote, getQuote] = useState("#");
+
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const { gallons, address, date, suggestedprice } = this.state;
-    console.log(gallons);
-    const Pricing = [gallons, address, date, suggestedprice];
+    console.log("Gallons:", gallons);
+    console.log("Delivery Date:", selectedDate);
+    const Pricing = [gallons, selectedDate];
     axios
-      .post("http://localhost:5001/home", Pricing)
-      .then(() => console.log("info sent"))
-      .catch((err) => {
-        console.error(err);
-      });
-  };
-  render() {
-    return (
-      <>
-        <Navbar />
-        <div className="form">
-          <h1>Fuel Quote Form</h1>
-          <form onSubmit={this.handleSubmit}>
-            <label htmlFor="gallons">Number of Gallons Requesting:</label>
-            <input
-              type="number"
-              name="gallons"
-              value={this.state.gallons}
-              onChange={this.handleInputChange}
-              required
-            />
-            <br />
-            <label htmlFor="location">Delivery Location: </label>
-            <input
-              type="text"
-              name="address"
-              value={this.state.address}
-              readOnly
-            />
-            <br />
-            <label htmlFor="date">Delivery Date:</label>
-            <br />
-            <DatePicker
-              name="date"
-              selected={this.state.date}
-              onChange={this.handleChange}
-              dateFormat="MM/dd/yyyy"
-              required
-            />
-            <br />
-            <label htmlFor="quote">Suggested price per gallon:</label>
-            <input
-              type="text"
-              name="quote"
-              value={this.state.suggestedprice}
-              readOnly
-            />
-            <br />
-            <label htmlFor="amountDue">Amount Due:</label>
-            <input type="text" name="amountdue" value="*" readOnly />
-            <br />
-            <input type="submit" value="Submit" />
-            <br />
-          </form>
-        </div>
-      </>
-    );
-  }
+        .post("http://localhost:5001/api/PricingModule", Pricing)
+        .then(() => console.log("info sent"))
+        .catch((err) => {
+            console.error(err);
+          });
+      };
+    const GetAddress = () => {
+    axios.get("http://localhost:5001/api/PricingModule")
+    .then((response)=>{setAddress(response.data)
+    })
+    .catch((error) => 
+    {
+        console.log(error);
+    })
+    };
+ 
+  return (
+    <div className="form">
+    {GetAddress()}
+      <h1>Fuel Quote Form</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="gallons">Number of Gallons Requesting:</label>
+        <input
+          type="number"
+          name="gallons"
+          value={gallons}
+          onChange={(e) => setGallons(e.target.value)}
+          required
+        />
+        <br />
+        <label htmlFor="location">Delivery Location: </label>
+        <input type="text" name="address" value={address} readOnly />
+        <br />
+        <label htmlFor="date">Delivery Date:</label>
+        <br />
+        <DatePicker
+          name="date"
+          selected={selectedDate}
+          onChange={(date) => setSelectedDate(date)}
+          dateFormat="MM/dd/yyyy"
+          required
+        />
+        <br />
+        <label htmlFor="quote">Suggested price per gallon:</label>
+        <input type="text" id="quote" value={quote} readOnly />
+        <br />
+        <label htmlFor="amountDue">Amount Due:</label>
+        <input type="text" id="amountdue" value={quote} readOnly />
+        <br />
+        <input type="submit" value="Submit" />
+        <br />
+      </form>
+    </div>
+  );
 }
 
 export default Pricing;
