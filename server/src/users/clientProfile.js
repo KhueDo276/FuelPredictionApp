2e844d51b065c4bd2f0e54243f9be5285af93134
quinoesteban555 <69019687+quinoesteban555@users.noreply.github.com>
2023-07-21 10:56:49 -0500

    Update clientProfile.js

--- server/src/users/clientProfile.js
+++ server/src/users/clientProfile.js
@@ -1,39 +1,20 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+import mysql from "mysql";
+export const ProfileManagementUpdate = async (req, res) => {
+  let Info = ["EJ5kMJOC0X"];
+  for (let i = 0;i < req.body.length;i++)
+  {
+    Info.push(req.body[i]);
+  }
+  console.log(Info);
+  var conn = mysql.createConnection({host:"fuelappthing.mysql.database.azure.com", user:"quinoesteban555", password:"@FuelAppPrediction", database:"fueldatabase", port:3306,});
+  conn.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "INSERT INTO profile (idprofile,Full_Name,Address_one,Address_two,City,State,Zipcode) VALUES ('"+Info[0]+"','"+Info[1]+"','"+Info[2]+"','"+Info[3]+"','"+Info[4]+"','"+Info[5]+"','"+Info[6]+"')";
+      conn.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Success");
+      });
+    });
+};
 
-const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.post("/api/save-client-profile", (req, res) => {
-  const {
-    lastName,
-    firstName,
-    id,
-    address1,
-    address2,
-    zipcode,
-    city,
-    selectedState,
-  } = req.body;
-
-  // Perform any necessary database or file operations to save the data
-  // Replace the console.log statements with your own logic
-
-  console.log("Last Name:", lastName);
-  console.log("First Name:", firstName);
-  console.log("ID:", id);
-  console.log("Address 1:", address1);
-  console.log("Address 2:", address2);
-  console.log("Zipcode:", zipcode);
-  console.log("City:", city);
-  console.log("State:", selectedState);
-
-  // Send a response indicating the successful submission
-  res.json({ message: "Client profile saved successfully" });
-});
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
